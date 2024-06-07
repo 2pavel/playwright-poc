ce7c23d1fb1bd1cc85495cfdd1e9a7ca08571534
Pawel Bialczak <pawelnwz@gmail.com>
2024-06-07 13:07:44 +0200

    adding setup and first page object

---
+++ page-object/landingPage.ts
@@ -0,0 +1,11 @@
+import { Locator, Page } from "@playwright/test";
+
+export class LandingPage {
+  readonly page: Page
+  readonly loginButton: Locator
+
+  constructor (page: Page) {
+    this.page = page
+    this.loginButton = page.getByRole('link', { name: 'Log in' })
+  }
+}
--- playwright.config.ts
+++ playwright.config.ts
@@ -32,6 +32,8 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
+    { name: 'setup', testMatch: /.*\.setup\.ts/ },
+    
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
