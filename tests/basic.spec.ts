33fa49c27db8c7b5bac2c82cc47fa3cc71b5de57
Pawel Bialczak <pawelnwz@gmail.com>
2024-06-07 17:37:55 +0200

    comment

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -11,7 +11,7 @@ test.describe('test', () => {
 
     // TODO: - storage authentication does not work
     //       - make sure that it is not possible to set it up this way
-    //       - try beforeEach hook using the storageState (simply click Log In on landing page)
+    //       - try beforeEach hook using the storageState (simply click Log In on landing page after setup)
     //       - if it does not work, set up login before each test
   })
 })
